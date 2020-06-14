2071f7d6516ff243ad05736245cef82f51056370
emil nordling <ekala001@gmail.com>
2020-06-14 19:12:03 +0200

    Fixes static files being served wrong

--- src/internal_host/main.ts
+++ src/internal_host/main.ts
@@ -3,20 +3,21 @@ import * as path from 'path';
 import cors from 'cors';
 
 const PORT = 8080;
-const STATIC = 'dist';
+const PUBLIC = path.join(__dirname, 'public');
 
 const app = express();
 
 app.use(cors());
 
-app.use(express.static(`${path.resolve('./')}/${STATIC}/www`));
+app.use(express.static(`${PUBLIC}`));
+app.use('/scripts', express.static(`${PUBLIC}`));
 
 app.get('/api', (_: Request, res: Response): void => {
   res.send('not yet implemented ...');
 });
 
 app.get('*', (_: Request, res: Response): void => {
-  res.sendFile(`${path.resolve('./')}/${STATIC}/index.html`);
+  res.sendFile(`${PUBLIC}/index.html`);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
